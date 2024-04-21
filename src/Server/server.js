b101b0afd455cfaef62933140610313745cece99
Rocket_Laggon <115794019+Abhinav2004arya@users.noreply.github.com>
2024-04-21 23:40:16 +0530

    Update server.js

--- src/Server/server.js
+++ src/Server/server.js
@@ -8,8 +8,8 @@ const app= express()
 app.use(express.json())
 app.use(cors())
 
-// mongoose.connect("mongodb+srv://aryaabhinav219:123@alphaclothing.ywv2tqg.mongodb.net/")
-mongoose.connect("mongodb://127.0.0.1:27017/AlphaClothing");
+mongoose.connect("mongodb+srv://aryaabhinav219:123@alphaclothing.ywv2tqg.mongodb.net/")
+// mongoose.connect("mongodb://127.0.0.1:27017/AlphaClothing");
 
 
 app.post('/register',(req,res) => {
